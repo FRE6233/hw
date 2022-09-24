42de329cf9a9e59701f37bb2f969f6eabe2ff64c
keithalewis <kal@kalx.net>
2022-09-24 19:02:07 +0000

    hw4

--- hw4/code/fms_option.h
+++ hw4/code/fms_option.h
@@ -1,6 +1,10 @@
 // fms_option.h - Forward option pricing model F = f e^{-kappa(s) + sX}
 #pragma once
 #include <cmath>
+#include <algorithm>
+#ifdef _DEBUG
+#include <random>
+#endif
 #include <tuple>
 #include "fms_distribution.h"
 #include "fms_distribution_normal.h"
@@ -14,19 +18,34 @@ namespace fms::option {
 	// F <= k iff X <= (log(k/f) + kappa(s))/s
 	inline double moneyness(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
+		if (k <= 0 || f <= 0 || s <= 0) {
+			return NaN;
+		}
+
 		return (log(k / f) + m.cgf(s)) / s;
 	}
 
 	// E[max(k - F, 0)] = k P(F <= k) - f P^s(F <= k)
 	inline double put(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
+		// edge cases
+		if (f == 0) {
+			return k;
+		}
+		if (s == 0) {
+			return std::max(k - f, 0.);
+		}
+		if (k == 0) {
+			return 0;
+		}
+
 		double x = moneyness(f, s, k, m);
 		double N = m.cdf(x);
 		double Ns = m.cdf(x, s);
 
 		return k * N - f * Ns;
 	}
-	// c - p = f - k
+	// Put call parity c - p = f - k
 	inline double call(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
 		return put(f, s, k, m) + f - k;
@@ -53,12 +72,12 @@ namespace fms::option {
 	}
 
 	// p = put(f, implied(f, p, k, m), k, m)
-	inline double implied(double f, double p, double k, double s = 0, unsigned n = 0, double tol = 0)
+	inline std::tuple<double,double,unsigned> implied(double f, double p, double k, double s = 0, unsigned n = 0, double tol = 0)
 	{
 		// max(k - f,0) >= k - f
 		// max(k - f,0) <= k
 		if (p <= std::max(k - f, 0.) || p >= k) {
-			return NaN;
+			return { NaN, 0, 0 };
 		}
 
 		if (s == 0) {
@@ -74,39 +93,46 @@ namespace fms::option {
 		double p_ = put(f, s, k);
 		double dp_ = vega(f, s, k); // dv/ds
 		double s_ = s - (p_ - p) / dp_; // Newton-Raphson
-		if (s_ < 0) {
-			s_ = s / 2;
-		}
-		while (fabs(s_ - s) > tol) {
+		s_ = std::clamp(s_, s / 2, 2 * s);
+
+		unsigned n0 = 1;
+		while (fabs(s_ - s) > tol && n0 <= n) {
 			p_ = put(f, s_, k);
 			dp_ = vega(f, s_, k);
 			s = s_ - (p_ - p) / dp_;
-			if (s < 0) {
-				s = s_ / 2;
-			}
+			s_ = std::clamp(s_, s / 2, 2 * s);
 			std::swap(s_, s);
-			if (n == 0) {
-				return NaN;
-			}
-			--n;
+			++n0;
 		}
 
-		return s_;
+		return { s_, s_ - s, n0 };
 	}
 #ifdef _DEBUG
 	inline bool test_implied()
 	{
+		std::default_random_engine dre;
+		auto rng = std::uniform_real_distribution(-.1, .1);
 		{
 			double f = 100;
 			double s = 0.2;
-			double k = 100;
-			double p = put(f, s, k);
-			
-			double s_ = implied(f, p, k);
-			if (fabs(s_ - s) > sqrt(eps)) {
+
+			unsigned nmax = 0;
+			double kmax = 0;
+			for (double k = 20; k <= 130; k += 10) {
+				double p = put(f, s, k);
+				auto [s_, ds, n] = implied(f, p, k, s + rng(dre), 100, sqrt(eps));
+				if (isnan(s_) || fabs(ds) > sqrt(eps) || n == 100) {
+					return false;
+				}
+				if (n > nmax) {
+					nmax = n;
+					kmax = k;
+				}
+			}
+			nmax = nmax;
+			if (put(f, s, 10) != 0) {
 				return false;
 			}
-
 		}
 
 		return true;
