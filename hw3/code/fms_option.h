34fd17d6b074317a273c7466f075de382f565b91
keithalewis <kal@kalx.net>
2022-09-24 08:51:38 +0000

    bs/m options remove xll

--- hw3/code/fms_option.h
+++ hw3/code/fms_option.h
@@ -1,18 +1,20 @@
 // fms_option.h - Forward option pricing model F = f e^{-kappa(s) + sX}
 #pragma once
 #include <cmath>
+#include <tuple>
 #include "fms_distribution.h"
+#include "fms_distribution_normal.h"
 
 namespace fms::option {
 
 	// F <= k iff X <= (log(k/f) + kappa(s))/s
-	double moneyness(double f, double s, double k, const distribution& m = distribution_normal{})
+	inline double moneyness(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
 		return (log(k / f) + m.cgf(s)) / s;
 	}
 
 	// E[max(k - F, 0)] = k P(F <= k) - f P^s(F <= k)
-	double put(double f, double s, double k, const distribution& m = distribution_normal{})
+	inline double put(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
 		double x = moneyness(f, s, k, m);
 		double N = m.cdf(x);
@@ -20,21 +22,26 @@ namespace fms::option {
 
 		return k * N - f * Ns;
 	}
-
+	// c - p = f - k
+	inline double call(double f, double s, double k, const distribution& m = distribution_normal{})
+	{
+		return put(f, s, k, m) + f - k;
+	}
+#if 0
 	// dp/df
-	double delta(double f, double s, double k, const distribution& m = distribution_normal{})
+	inline double delta(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
 		return 0;
 	}
 
 	// d^2p/df^2
-	double gamma(double f, double s, double k, const distribution& m = distribution_normal{})
+	inline double gamma(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
 		return 0;
 	}
-
+#endif // 0
 	// dp/ds
-	double vega(double f, double s, double k, const distribution& m = distribution_normal{})
+	inline double vega(double f, double s, double k, const distribution& m = distribution_normal{})
 	{
 		double x = moneyness(f, s, k, m);
 
@@ -42,7 +49,7 @@ namespace fms::option {
 	}
 
 	// p = put(f, implied(f, p, k, m), k, m)
-	double implied(double f, double p, double k, double s = 0, unsigned n = 0, double tol = 0)
+	inline double implied(double f, double p, double k, double s = 0, unsigned n = 0, double tol = 0)
 	{
 		static const double NaN = std::numeric_limits<double>::quiet_NaN();
 		static const double eps = std::numeric_limits<double>::epsilon();
@@ -95,7 +102,7 @@ namespace fms::option {
 			double k = 100;
 			double p = put(f, s, k);
 			
-			double s_ = implied(f, s, k);
+			double s_ = implied(f, p, k);
 			if (fabs(s_ - s) > sqrt(eps)) {
 				return false;
 			}
@@ -105,4 +112,54 @@ namespace fms::option {
 		return true;
 	}
 #endif // _DEBUG
+
+	// Black-Scholes/Merton 
+	// S_t = S exp(r - kappa(sigma sqrt(t)) + sigma sqrt(t) X)
+	// F_t = f exp(-kappa(s) + s X)
+	// so D = exp(-rt), f = S/D, s = sigma sqrt(t)
+	namespace bsm {
+
+		struct put {
+			double k;
+			double t;
+		};
+		struct call {
+			double k;
+			double t;
+		};
+
+		// Black to Black-Scholes/Merton parameters
+		inline std::tuple<double, double, double> Dfs(double r, double S, double sigma, double t)
+		{
+			double D = exp(-r * t);
+
+			return { D, S / D, sigma * sqrt(t) };
+		}
+
+		// value = D*E[nu(F/D)]
+		template<class O>
+		inline double value(double r, double S, double sigma, const O& o, const distribution& m = distribution_normal{});
+		
+		template<>
+		inline double value(double r, double S, double sigma, const put& o, const distribution& m)
+		{
+			auto [D, f, s] = Dfs(r, S, sigma, o.t);
+
+			return D * option::put(f, s, o.k, m);
+		}
+
+		template<>
+		inline double value(double r, double S, double sigma, const call& o, const distribution& m)
+		{
+			auto [D, f, s] = Dfs(r, S, sigma, o.t);
+
+			return D * option::call(f, s, o.k, m);
+		}
+
+		// delta
+		// gamma
+		// vega
+		// implied
+
+	}
 }
